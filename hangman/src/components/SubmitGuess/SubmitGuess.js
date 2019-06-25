2f68edb557192ad106e4bb82ccb53bd09211246d
Dyanna Tolman <dyannast@gmail.com>
2019-06-25 15:30:46 -0700

    letter guess checking correctly against word array and displaying if exist in word

--- hangman/src/components/SubmitGuess/SubmitGuess.js
+++ hangman/src/components/SubmitGuess/SubmitGuess.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import './SubmitGuess.css';
 
 class SubmitGuess extends Component {
+  handleSubmit = (event) =>{
+    event.preventDefault()
+    const letter = this.refs.textInput.value
+    this.props.updateGuessLetters(letter)
+    this.refs.textInput.value = ''
+  }
   render() {
     return (
     <div className="SubmitGuess">
-      <form>
-        <input type="text" maxLength='1'/>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" ref="textInput" maxLength='1'/>
         <button>Submit</button>
       </form>
     </div>
